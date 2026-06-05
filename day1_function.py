ea4b803ec65d47e8c48c0d2686d771f5aa94b5b3
kuroi042 <bouderraoui45@gmail.com>
2026-06-05 11:44:22 +0100

    setup

---
+++ day1_function.py
@@ -0,0 +1,30 @@
+# def price_for_mileage(milles):
+#     price  = 240000 - 0.05*milles
+#     return int(price)
+
+# print(price_for_mileage(0))
+# print(price_for_mileage(100000))
+# print(price_for_mileage(200000))
+
+
+# m =-0.05 ##* every extra kilo reducdes the price by 0.05
+# b = 20000
+# for x in [0,50000,10000, 150000]:
+#         y = m*x+b
+#         print(f"x={x}->y={y}")
+
+#* x=0->y=20000.0
+#* x=50000->y=17500.0
+#* x=10000->y=19500.0
+#* x=150000->y=12500.0
+
+
+##* compute the slope manually
+# these two points
+# x1 = 100000
+# y1=15000
+# x2 = 200000 
+# y2=10000
+
+# slope =  (y2-y1)/(x2-x1)
+# print (slope)
