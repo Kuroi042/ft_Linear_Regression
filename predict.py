a34fba4b09a722ced03842bb94590f94ddfb529c
Mohamed charaf Bouderraoui <mbouderr@c1r5p10.1337.ma>
2026-06-15 18:37:13 +0100

    done flake

--- predict.py
+++ predict.py
@@ -1,40 +1,40 @@
-import sys , os
-import numpy as np 
+import sys
+import os
 import matplotlib.pyplot as plt
 import json
 from pathlib import Path
 file_path = os.path.join('Weights.json')
 
+
 def execute(km, path):
-    try:
-        with open('weights.json' ,'r') as file:
-            data =  json.load(file)
-    except:
-        print(f"error '{path}' not found ")
-        sys.exit(1)
-    theta0 =  data['theta0']
-    theta1 =  data['theta1']
-    km_min  = data['km_min']
-    km_max  = data['km_max']
+
+    with open('weights.json', 'r') as file:
+        data = json.load(file)
+    theta0 = data['theta0']
+    theta1 = data['theta1']
+    km_min = data['km_min']
+    km_max = data['km_max']
 
     km_scaled = (km - km_min) / (km_max - km_min)
 
-    price  = theta0 + km_scaled*theta1
+    price = theta0 + km_scaled*theta1
     print(f"price for car of {km} km : {price}")
     plt.plot(price)
 
 
 def main():
     try:
-        assert len(sys.argv) == 2 , "argument are bad"
-    except AssertionError as e :
+        assert len(sys.argv) == 2, "argument are bad"
+    except AssertionError as e:
         print(f"AssertionError: {e}")
         sys.exit(1)
-    mileage =  int(sys.argv[1])
-    weight =  Path("weights.json")
-    if  not weight.is_file() :
+    mileage = int(sys.argv[1])
+    weight = Path("weights.json")
+    if not weight.is_file():
         print("File does not exist!")
         sys.exit(1)
     execute(mileage, weight)
+
+
 if __name__ == "__main__":
     main()
--- train.py
+++ train.py
@@ -8,45 +8,40 @@ import json
 
 class params:
     def __init__(self, path):
-        self.df =  pd.read_csv(path)
-        self.slop =None
-        self.theta0 = 0 #*intercept
-        self.theta1 = 0 #*slop 
+        self.df = pd.read_csv(path)
+        self.slop = None
+        self.theta0 = 0
+        self.theta1 = 0
         self.cost = 0
-        self.costarray= []
+        self.costarray = []
         self.cost_diff = []
-        self.mse= 0
+        self.mse = 0
 
-#* tmpθ0 = learningRate ∗ 1/m  (estimatePrice(mileage[i]) − price[i])
-#* tmpθ1 = learningRate ∗ 1/m (estimatePrice(mileage[i]) − price[i]) ∗ mileage[i]
     def gredient_descent(self):
-        Lr  =  0.01
+        Lr = 0.01
         self.theta0 = float(self.theta0)
         self.theta1 = float(self.theta1)
         km_min = self.df["km"].min()
         km_max = self.df["km"].max()
         km_min = float(km_min)
         km_max = float(km_max)
-        self.df["km_scaled"] = (self.df["km"] - km_min) / (km_max - km_min)        
-        m =  self.df.shape[0]
+        self.df["km_scaled"] = (self.df["km"] - km_min) / (km_max - km_min)
+        m = self.df.shape[0]
         for i in range(10000):
-            #*y^​=θ0​+θ1​x
-            # temp = 0
             estimatePrice0 = self.theta0 + (self.theta1*self.df["km_scaled"])
-            sum_error = (estimatePrice0 -self.df["price"])
+            sum_error = (estimatePrice0 - self.df["price"])
             gradient_theta0 = (1/m)*(sum_error).sum()
-            gradient_theta1 = (1/m)*(sum_error *self.df["km_scaled"]).sum()
+            gradient_theta1 = (1/m)*(sum_error * self.df["km_scaled"]).sum()
             temptheta0 = self.theta0 - (Lr * gradient_theta0)
             temptheta1 = self.theta1 - (Lr * gradient_theta1)
-            self.theta0 =  temptheta0
+            self.theta0 = temptheta0
             self.theta1 = temptheta1
             self.cost = (sum_error**2).sum() / (2 * m)
-            
             self.costarray.append(self.cost)
-            self.cost_diff.append(-self.costarray[i] +self.costarray[i-1])
-            print("iteration" , i)
-        print("cost diff"  , self.cost_diff , "\n")
-        self.mse =  ((sum_error)**2).mean()
+            self.cost_diff.append(-self.costarray[i] + self.costarray[i-1])
+            print("iteration", i)
+        # print("cost diff", self.cost_diff, "\n")
+        self.mse = ((sum_error)**2).mean()
         with open("weights.json", "w") as json_file:
             json.dump({
                 "theta0": self.theta0,
@@ -54,7 +49,6 @@ class params:
                 "km_min": km_min,
                 "km_max": km_max
             }, json_file, indent=4)
-
 
     def plotregree(self):
         newprice = self.theta0 + self.theta1 * self.df["km_scaled"]
@@ -66,8 +60,12 @@ class params:
         ax[0].set_xlabel("km")
         ax[0].set_ylabel("price")
 
-        ax[1].scatter(self.df["km"], self.df["price"], color="blue", label="Actual")
-        ax[1].plot(self.df["km"], newprice, color="red", label="Prediction")
+        ax[1].scatter(
+            self.df["km"], self.df["price"],
+            color="blue", label="Actual")
+        ax[1].plot(
+            self.df["km"], newprice, color="red",
+            label="Prediction")
         ax[1].set_title("Linear Regression")
         ax[1].legend()
 
@@ -84,7 +82,7 @@ class params:
 
         r2 = 1 - (ss_res / ss_tot)
         if r2 > 0.6:
-            txt =  "Model Passed"
+            txt = "Model Passed"
         else:
             txt = "Model failed"
         ax[3].text(
@@ -99,32 +97,31 @@ class params:
         plt.tight_layout()
         plt.show()
 
+
 def execute(algo):
     algo.gredient_descent()
-# bonus
     algo.plotregree()
+
+
 def main():
     try:
-        assert len(sys.argv) == 2 , "argument are bad"
-    except AssertionError as e :
+        assert len(sys.argv) == 2, "argument are bad"
+    except AssertionError as e:
         print(f"AssertionError: {e}")
         sys.exit(1)
-    path  = Path(str(sys.argv[1]))
+    path = Path(str(sys.argv[1]))
     print(path)
-    tools =  params(path)
+    tools = params(path)
     with open("weights.json", "w") as json_file:
         json.dump({
                 "theta0": 0,
                 "theta1": 0,
                 "km_min": 0,
                 "km_max": 0
-            }, json_file, indent=4)  
+            }, json_file, indent=4)
 
     execute(tools)
+
+
 if __name__ == "__main__":
     main()
-
-
-
-
-  
